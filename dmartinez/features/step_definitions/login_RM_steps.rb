e4ec6a0024a6b4ed6a2f89c045d6beeaacf834c8
Daniel Zenoss <danielzenoss@Daniels-MacBook-Pro.local>
2017-12-28 13:08:25 -0600

    updates after talking to Alvaro

--- dmartinez/features/step_definitions/login_RM_steps.rb
+++ dmartinez/features/step_definitions/login_RM_steps.rb
@@ -1,31 +1,31 @@
 Given("I am located within the Zenoss Resmgr Login Page") do
-	visit("/")
+    visit("/")
 end
 
 When("I enter an incorrect username") do
-  fill_in('username', :with => 'fake22')
+    fill_in('username', :with => 'fake22')
 end
 
 When("I enter an incorrect password") do
-  fill_in('passwrd', :with => 'Zenoss222')
+    fill_in('passwrd', :with => 'Zenoss222')
 end
 
 When("I enter the submit button") do
-  click_button('loginButton')
+    click_button('loginButton')
 end
 
 Then("A message should appear") do
-  page.has_xpath?('//*[contains(@class, "error")]')
+    page.has_xpath?('//*[contains(@class, "error")]')
 end
 
 When("I don't enter a Username") do
-  fill_in('username', :with => '')
+    fill_in('username', :with => '')
 end
 
 
 
 When("I enter a valid password") do
-  fill_in('passwrd', :with => 'Zenoss22')
+    fill_in('passwrd', :with => 'Zenoss22')
 end
 
 When("I don't enter a password") do
@@ -33,21 +33,21 @@ When("I don't enter a password") do
 end
 
 When("I enter a valid Username") do
-  fill_in('username', :with => 'admin')
+    fill_in('username', :with => 'admins')
 end
 
 When("I  enter a valid password") do
-  fill_in('passwrd', :with => 'Zenoss22')
+    fill_in('passwrd', :with => 'Zenoss22')
 end
 
 Then("the Dashboard page should be displayed") do
-  page.has_xpath?('//*[contains(@class, "bg")]')
+    page.has_xpath?('//*[contains(@class, "bg")]')
 end
 
 When("I click Log Out") do
-  page.find(:xpath,"//*[text()='#{SIGN OUT}']").click
+    click_link("sign out")
 end
 
 Then("I see the login page") do
-  page.has_xpath?('//*[contains(@class, "submitbutton")]')
+    page.has_xpath?('//*[contains(@class, "submitbutton")]')
 end
--- dmartinez/features/support/env.rb
+++ dmartinez/features/support/env.rb
@@ -1,14 +1,15 @@
 require 'capybara'
 require 'capybara/cucumber'
+require 'selenium-webdriver'
 
 Capybara.configure do |config|
 	config.app_host = 'https://10.88.120.20:54321'
-	config.default_driver = :selenium_chrome
+	config.default_driver = :selenium_firefox
 	config.default_max_wait_time = 10
 	config.default_selector = :xpath
 	config.run_server = false
 end
 
-Capybara.register_driver :selenium_chrome do |app|
-	Capybara::Selenium::Driver.new(app, browser: :chrome)
+Capybara.register_driver :selenium_firefox do |app|
+	Capybara::Selenium::Driver.new(app, browser: :firefox, marionette: true, desired_capabilities: Selenium::WebDriver::Remote::Capabilities.firefox(acceptInsecureCerts: true))
 end
