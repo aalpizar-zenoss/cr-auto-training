4f061f264793f9bb91955ac24072b9e2fbf59fd2
Yacdaniel Hurtado <yhurtado@zenoss.com>
2018-01-04 16:43:39 -0600

    add elements and use wait_for visible

--- yhurtado/features/pages/advance_page.rb
+++ yhurtado/features/pages/advance_page.rb
@@ -5,32 +5,42 @@ class AdvancePage < SitePrism::Page
   element :add_ldap_button, 'button#ldapaddbutton-btnEl'
   element :host_input, 'input[name="host_0"]'
   element :manager_dn_input, 'input[name="managerDN"]'
+  element :password_input, 'input[name="managerPassword"]'
+  element :user_base_dn_input, 'input[name="userBaseDN"]'
+  element :group_base_dn_input, 'input[name="groupBaseDN"]'
+  element :add_ldap_title, :xpath, '//span[text()="1. Add LDAP Servers"]'
+  element :configure_plugin_title, :xpath, '//span[text()="2. Configure LDAP Plugin"]'
+  element :map_ldap_title, :xpath, '//span[text()="3. Map LDAP Groups to Local Groups"]'
 
   def add_ldap_configuration host, manager_dn, password
-    wait_until_add_ldap_button_visible(10)
+    wait_until_add_ldap_button_visible
     add_ldap_button.click
     within('div.x-window-default-plain') do
-      fill_in 'host_0', with: host
-      sleep 0.3
-      fill_in 'managerDN', with: manager_dn
-      sleep 0.3
-      fill_in 'managerPassword', with: password
-      sleep 0.3
+      wait_until_add_ldap_title_visible
+      wait_until_host_input_visible
+      wait_until_manager_dn_input_visible
+      wait_until_password_input_visible
+      host_input.set host
+      manager_dn_input.set manager_dn
+      password_input.set password
       click_link_or_button('Next')
     end
   end
 
   def configure_ldap_plugin users_base_dn, groups_base_dn
     within('div.x-window-default-plain') do
-      fill_in 'userBaseDN', with: users_base_dn
-      sleep 0.3
-      fill_in 'groupBaseDN', with: groups_base_dn
+      wait_until_configure_plugin_title_visible
+      wait_until_user_base_dn_input_visible
+      wait_until_group_base_dn_input_visible
+      user_base_dn_input.set users_base_dn
+      group_base_dn_input.set groups_base_dn
       click_link_or_button('Next')
     end
   end
 
   def map_ldap_group group, role
     within('div.x-window-default-plain') do
+      wait_until_map_ldap_title_visible
       click_link_or_button('Finish')
     end
   end
