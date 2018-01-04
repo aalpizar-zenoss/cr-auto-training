f7456b9636e18a6ebd98dffff962eba38dd2aa8f
aalpizar-zenoss <aalpizar@zenoss.com>
2018-01-04 14:59:50 -0600

    Revert "voro updates"

--- dmartinez/features/step_definitions/add_linux_device_RM_steps.rb
+++ dmartinez/features/step_definitions/add_linux_device_RM_steps.rb
@@ -1,12 +1,12 @@
-Given(/^I am logged as an admin in RM using the username: (.*?) and the password: (.*?)$/) do |usr, passwd|
+Given("I am logged as an admin in RM using the username: admin and the password: Zenoss{int}") do |int|
     @login_page = LoginPage.new 
     @login_page.load
-    @login_page.username.set usr
-    @login_page.passwrd.set passwd
+    @login_page.username.set "admin"
+    @login_page.passwrd.set "Zenoss22"
     @login_page.login_button.click
 end
 
-When("I click on the \"Infrastructure\" tab of the RM application") do
+When("I click on the {string} tab of the RM application") do |string|
     @dashboard = DashboardPage.new
     @dashboard.load
     @dashboard.dashboard_page.click
@@ -17,34 +17,37 @@ Then("I should be able to see a list of all the RM devices") do
   expect(@infrastructure).to have_device_list
 end
 
-When("I click on the $btn button") do |string|
+When("I click on the {string} button") do |string|
   @infrastructure.add_devices_button.click
 end
 
-Then("I should see a list of $dev options") do |string|
+Then("I should see a list of {string} options") do |string|
   expect(@infrastructure).to have_add_device_options
 end
 
+When("I click on {string}") do |string|
+  @infrastructure.add_single_device.click
+end
+
 Then("I should be able to see a modal to enter device details") do
-  @infrastructure.wait_until_add_device_modal_visible
+  expect(@infrastructure).to have_add_device_modal
 end
 
-When("I  enter the Linux Server IP Address $ip") do |ip|
-    @infrastructure.device_ip_address.set ip
+When("I  enter the Linux Server IP Address {string}") do |string|
+    @infrastructure.device_ip_address.set string
 end
 
-Then("I should select the Device Class '$devclass'") do |devclass|
-  @infrastructure.device_class.set devclass
+Then("I should select the Device Class {string}") do |string|
+  @infrastructure.device_class.set string
 end
 
-Then("I should click on '$option'") do |option|
-  click_on option
+Then("I should click on {string}") do |string|
+  click_on string
 end
 
-Then("I should set as the zCommandUsername as '$usr' and the zCommandPassword '$passwd'") do |usr, passwd|
-  @infrastructure.wait_until_server_username_visible
-  @infrastructure.server_username.set usr
-  @infrastructure.server_password.set passwd
+Then("I should set as the zCommandUsername as {string} and the zCommandPassword {string}") do |string, string2|
+  @infrastructure.server_username.set string
+  @infrastructure.server_password.set string2
 end
 
 Then("I should click on the {string}") do |string|
@@ -55,9 +58,10 @@ Then("I should refresh the device list") do
  refresh_attempts = 10
  refresh_attempts.times do
   @infrastructure.refresh.click
- end
+end
 end
 
-When("I see the Infraestructure page displaying I should be able to see the Linux Server IP Address '$string' just added") do |string|
+When("I see the Infraestructure page displaying I should be able to see the Linux Server IP Address {string} just added") do |string|
   expect(@infrastructure).to have_xpath("//*[text()='"+string+"']")
+
 end
