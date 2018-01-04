c9c8a714f236db20c0a92dae3992dfd29c052e65
dnietozenoss <dnieto@zenoss.com>
2018-01-04 16:54:16 -0500

    Update advanced_page.rb

--- cucumber_dnieto/features/page_objects/advanced_page.rb
+++ cucumber_dnieto/features/page_objects/advanced_page.rb
@@ -1,4 +1,5 @@
 require 'site_prism'
+require_relative 'advanced_page_sections'
 
 class AdvancedPage < SitePrism::Page
 	set_url "/zport/dmd/manageLDAPConfig"
@@ -6,22 +7,12 @@ class AdvancedPage < SitePrism::Page
 	element :add_button, :css, "div[id='ldapaddbutton']"
 	element :finish_button, "//*[contains(text(),'Finish')]"
 
-	class AddLDAPConfigOverlay < SitePrism::Section
-		element :host_field, :css, "input[name='host_0']"
-		element :manager_dn, :css, "input[name='managerDN']"
-		element :manager_psw, :css, "input[name='managerPassword']"
-		element :next_button, "//*[contains(text(),'Next')]"
-	end
-
-	class ConfigLDAPluginOverlay < SitePrism::Section
-		element :user_base_dn, :css, "input[name='userBaseDN']"
-		element :group_base_dn, :css, "input[name='groupBaseDN']"
-		element :user_filter, :css, "input[name='extraUserFilter']"
-		element :group_filter, :css, "input[name='extraGroupFilter']"
-		element :next_button, "//*[contains(text(),'Next')]"
-	end
-
 	section :add_ldap, AddLDAPConfigOverlay, :css, 'div.x-window'
     section :config_ldap_plugin, ConfigLDAPluginOverlay, :css, 'div.x-window'
+
+    def add_login_name_attr attribute
+		xpath_device_class = '//li[contains(text(),"'+ attribute + '")]'
+		find(:xpath, xpath_device_class).click
+	end
 
 end
