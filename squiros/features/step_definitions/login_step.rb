bc96249925ae64e8fb8627d3ff429b23d44297ef
aalpizar <aalpizar@zenoss.com>
2018-03-14 15:17:00 -0600

    Adaptacines de la practica en CentOS

--- squiros/features/step_definitions/login_step.rb
+++ squiros/features/step_definitions/login_step.rb
@@ -2,7 +2,7 @@
 Given (/^I am on the Zenoss loginpage$/) do
   @login = LoginPage.new
   @login.load # visit("/")
-  expect(page).to have_title "Login"
+  expect(@login.title).to eq("Login")
 end
 
 When (/^enter this "(.*?)" invalid username$/) do |user|
@@ -24,7 +24,7 @@ Then (/^some error login is shown, like this "(.*?)"$/) do |error_msj|
   # puts find(:xpath, '//*[@id="errorbox"]/span').text
   # has_css?('.error')
   # has_content?('entered an incorrect username or password')
-  expect(page).to have_text error_msj
+  expect(@login).to have_text error_msj
 end
 
 When (/^enter this "(.*?)" valid password$/) do |pass|
@@ -51,7 +51,7 @@ Then (/^I am on the Zenoss page$/) do
   @dashboard = Dashboard.new
   @dashboard.load
   # page.has_title?('Zenoss: Dashboard')
-  expect(page).to have_title "Zenoss: Dashboard"
+  expect(@dashboard.title).to eq("Zenoss: Dashboard")
 end
 
 When (/^I click the SIGN OUT button$/) do
